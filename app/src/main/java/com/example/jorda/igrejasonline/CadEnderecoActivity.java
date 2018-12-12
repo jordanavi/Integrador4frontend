88529bcda07f7b7bb582e81c467eaf0257098bd4
jordanavi <jordanavilela01@gmail.com>
2018-12-12 13:37:38 -0200

    Tela de cadastro de endereco

---
+++ app/src/main/java/com/example/jorda/igrejasonline/CadEnderecoActivity.java
@@ -0,0 +1,20 @@
+package com.example.jorda.igrejasonline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CadEnderecoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_endereco);
+    }
+
+    public void voltarInicio(View view) {
+    }
+
+    public void concluir(View view) {
+    }
+}

9bdf56f503e7fd8c7523239375069a2f6c12f871
jordanavi <jordanavilela01@gmail.com>
2018-12-12 17:11:34 -0200

    Alteracoes no pacote service - ainda nao concluido

---
+++ app/src/main/java/com/example/jorda/igrejasonline/service/RetrofitService.java
@@ -0,0 +1,32 @@
+package com.example.jorda.igrejasonline.service;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+    public class RetrofitService {
+
+        private String baseUrl = "url/";
+        private ModeloApi api;
+        private static RetrofitService instancia;
+
+        private RetrofitService() {
+            api = criaRetrofit().create(ModeloApi.class);
+        }
+
+        private Retrofit criaRetrofit() {
+            Gson gson = new GsonBuilder().create();
+            return new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+
+        public static ModeloApi getServico() {
+            if (instancia == null)
+                instancia = new RetrofitService();
+            return instancia.api;
+        }
+    }
+}

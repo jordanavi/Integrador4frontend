4067a4b17884ac8114ada2114b407b454ab24e1e
jordanavi <jordanavilela01@gmail.com>
2018-12-12 12:22:34 -0200

    Alteracoes na tela cad_igreja

--- app/src/main/java/com/example/jorda/igrejasonline/CadIgrejaActivity.java
+++ app/src/main/java/com/example/jorda/igrejasonline/CadIgrejaActivity.java
@@ -2,6 +2,7 @@ package com.example.jorda.igrejasonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CadIgrejaActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class CadIgrejaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_igreja);
+    }
+
+    public void cadastrarIgreja(View view) {
+    }
+
+    public void voltarInicio(View view) {
     }
 }
